e3c18d351f242eaab7f1d17f42a8d1de6e3eb667
afterfireworks <afterfireworks@users.noreply.github.com>
2020-11-28 01:27:58 +0800

    20201128 0127

--- src/index.js
+++ src/index.js
@@ -9,12 +9,34 @@
     // } else if (html.webkitRequestFullscreen) {
     //     html.webkitRequestFullscreen();
     // }
-    window.addEventListener("load",function() {
+    var win = window,
+      doc = win.document;
+
+  // If there's a hash, or addEventListener is undefined, stop here
+  if ( !location.hash || !win.addEventListener ) {
+
+    //scroll to 1
+    window.scrollTo( 0, 1 );
+    var scrollTop = 1,
+
+    //reset to 0 on bodyready, if needed
+    bodycheck = setInterval(function(){
+      if( doc.body ){
+        clearInterval( bodycheck );
+        scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
+        win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+      } 
+    }, 15 );
+
+    if (win.addEventListener) {
+      win.addEventListener("load", function(){
         setTimeout(function(){
-            // This hides the address bar:
-            window.scrollTo(0, 1);
+          //reset to hide addr bar at onload
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
         }, 0);
-    });
+      }, false );
+    }
+  }
 
     function animateNav() {
         const nav = document.querySelector('nav');
