fa6649e5f91247c75b0443819a7d0ce3d21409ce
afterfireworks <afterfireworks@users.noreply.github.com>
2020-12-04 22:47:21 +0800

    20201204 2247

--- src/MaterializeInit.js
+++ src/MaterializeInit.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     let sidenav = document.querySelectorAll('.sidenav');
     let sidenavinstances = M.Sidenav.init(sidenav, {
         edge: 'left',
         preventScrolling: true,
     });
+
 
     let slider = document.querySelectorAll('.slider');
     let sliderinstances = M.Slider.init(slider, {
@@ -11,19 +13,26 @@ document.addEventListener('DOMContentLoaded', function () {
         interval: 5000,
     });
 
-    let scrollspy = document.querySelectorAll('.scrollspy');
-    let scrollspyinstances = M.ScrollSpy.init(scrollspy);
 
-    
-    let pushpin = document.querySelectorAll('.pushpin');
-    let pushpininstances = M.Pushpin.init(pushpin);
-     
+    let scrollspy = document.querySelectorAll('.scrollspy');
+    let scrollspyinstances = M.ScrollSpy.init(scrollspy,{
+        scrollOffset: 400,
+    });
+
+
+    // let pushpin = document.querySelectorAll('.pushpin');
+    // let pushpininstances = M.Pushpin.init(pushpin, {
+    //     offset: 0,
+    // });
+
 
     let tabs = document.querySelector('.tabs');
     let tabsinstances = M.Tabs.init(tabs);
 
+
     let materialboxed = document.querySelectorAll('.materialboxed');
     let materialboxedinstances = M.Materialbox.init(materialboxed);
+
 
     let carouselSlider = document.querySelectorAll('.carousel-slider');
     let carouselSliderinstances = M.Carousel.init(carouselSlider, {
@@ -31,8 +40,10 @@ document.addEventListener('DOMContentLoaded', function () {
         indicators: true,
     });
 
+    
     let parallax = document.querySelectorAll('.parallax');
     let parallaxinstances = M.Parallax.init(parallax);
+
 
     let fixedbtn = document.querySelectorAll('.fixed-action-btn');
     let fixedbtninstances = M.FloatingActionButton.init(fixedbtn, {
