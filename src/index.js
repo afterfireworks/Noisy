026d5f076b5df38486e3faa8e3287a2da36326b2
afterfireworks <afterfireworks@users.noreply.github.com>
2020-12-04 23:05:34 +0800

    20201204 2305

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
     function animateNav() {
         const nav = document.querySelector('nav');
         const navHeight = nav.offsetHeight;
-        if (window.pageYOffset > navHeight) { return nav.classList.add('grey', 'darken-4', 'shadow') }
-        nav.classList.remove('grey', 'darken-4', 'shadow')
+        if (window.pageYOffset > navHeight) { return nav.classList.add('shadow') }
+        nav.classList.remove('shadow')
     }
 
     window.addEventListener('scroll', () => {
