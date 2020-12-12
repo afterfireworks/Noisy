a19650e0b2a47c24e4d0fd89234c520c66e26c09
afterfireworks <afterfireworks@users.noreply.github.com>
2020-12-12 17:45:44 +0800

    20201212 1745

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 (function () {
    
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  // window.addEventListener('resize', () => {
+  //   // We execute the same script as before
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // });
 
     function animateNav() {
         const nav = document.querySelector('nav');
