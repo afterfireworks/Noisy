21fb0e7566764d8cfb08ffac2987b1a8834bed5a
afterfireworks <afterfireworks@users.noreply.github.com>
2021-02-07 17:05:59 +0800

    20210207 upload NoisyPlayer

---
+++ src/Extend/Basic/NoisyPlayer/CtxInit.js
@@ -0,0 +1,38 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+
+const source = audioCtx.createMediaElementSource(audio);
+const analyser = audioCtx.createAnalyser();
+const stereoPanner = audioCtx.createStereoPanner();
+const delayGain = audioCtx.createGain();
+const delayFilter = audioCtx.createBiquadFilter();
+const delayTime = audioCtx.createDelay();
+const reverb = audioCtx.createConvolver();
+const masterVolume = audioCtx.createGain();
+// convolver.buffer = await getImpulseBuffer(audioCtx)
+
+
+
+// Master Chain
+source.connect(analyser);
+analyser.connect(stereoPanner);
+stereoPanner.connect(masterVolume);
+// Delay Chain
+stereoPanner.connect(delayGain);
+delayGain.connect(delayFilter);
+delayFilter.connect(delayTime);
+delayTime.connect(masterVolume);
+// Output
+masterVolume.connect(audioCtx.destination);
+masterVolume.gain.value = 0.05
+
+const Init = document.getElementById("init");
+Init.addEventListener('click', () => {
+    Init.style.transition = "all .6s"
+    Init.style.marginLeft = "-100vw"
+    audioCtx.resume().then(() => {
+        console.log('Playback resumed successfully');
+    });
+})
+
+
---
+++ src/Extend/Basic/NoisyPlayer/index.js
@@ -0,0 +1,416 @@
+const SongArray = [
+    ["WeLostTheSea-AGallantGentleman-LiveatStudios301.mp3", "WeLostTheSea.artist.jfif", "WeLostTheSea.03.jpg", "We Lost The Sea - A Gallant Gentleman - Live at Studios 301"],
+    ["NoPartyForCaoDong-Wayfarer.mp3", "NoPartyForCaoDong.artis.jpg", "NoPartyForCaoDong.no1.jpg", "草東沒有派對 - 山海"],
+    ["NoPartyForCaoDong-Devotion.mp3", "NoPartyForCaoDong.artis.jpg", "NoPartyForCaoDong.Devotion.jpg", "草東沒有派對 - 還願"],
+    ["Vast&Hazy-IntheDark.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ep01.jpg", "Vast & Hazy - 無差別傷害"]
+]
+
+function isMobile() {
+
+    try { document.createEvent("TouchEvent"); return true; }
+
+    catch (e) { return false; }
+
+}
+
+const audio = document.querySelector('audio');
+const canvas = document.querySelector('canvas');
+
+const Music = document.getElementById('music');
+const PlayInfo = document.getElementById('playInfo');
+const PlayProgressBar = document.getElementById("playProgressBar")
+const CurTime = document.getElementById("curTime")
+const FullTime = document.getElementById("fullTime")
+const SongBook = document.getElementById('songBook');
+const SongSelected = document.getElementById('songSelected');
+
+const CtxSwitch = document.getElementById('ctxSwitch');
+const ListSwitch = document.getElementById('listSwitch');
+const ContorlsPanel = document.getElementById('contorlsPanel');
+const CtxContorls = document.getElementById('ctxContorls');
+const ListContorls = document.getElementById('listContorls');
+
+const SongName = document.getElementById('songName')
+const BtnPrev = document.getElementById("btnPrev");
+const BtnPrevTime = document.getElementById("btnPrevTime");
+const BtnPlay = document.getElementById("btnPlay");
+const BtnStop = document.getElementById("btnStop");
+const BtnNextTime = document.getElementById("btnNextTime");
+const BtnNext = document.getElementById("btnNext");
+const MusicVolume = document.getElementById("musicVolume");
+const VolumeControl = document.getElementById("volumeControl");
+const VolumeValue = document.getElementById("volumeValue");
+const ArtistImg = document.getElementById("artistImg");
+const AlbumImg = document.getElementById("albumImg");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// SongBook
+// const SongSelect = document.getElementById('songSelect')
+// function createSongBook() {
+//     let list = "";
+
+//     for (i = 0; i < SongSelected.length; i++) {
+//         list = document.createElement('div');
+//         list.className = 'bePick'
+//         list.innerText = SongSelected.options[i].innerText.trim();
+//         SongSelect.appendChild(list);
+//     }
+//     for (i = 1; i <= 10; i++) {
+//         list = document.createElement("div");
+//         list.innerText = "Song " + (SongSelected.length + i);
+//         SongSelect.appendChild(list);
+//     }
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// ContorlsPanel Switch
+ListSwitch.addEventListener('click', () => {
+    let MmarginTop = listContorls.style.marginTop;
+
+    if (MmarginTop === "-100vh") {
+        listContorls.style.marginTop = "0px";
+    } else {
+
+        if (isMobile()) {
+            PlayInfo.classList.toggle("notDesktop");
+            ContorlsPanel.classList.toggle("visiable");
+        } else {
+            playInfo.classList.toggle("vw50");
+            ContorlsPanel.classList.toggle("vw50");
+        }
+        listContorls.style.marginTop = "0px";
+    }
+})
+
+CtxSwitch.addEventListener('click', () => {
+    let MmarginTop = listContorls.style.marginTop;
+
+    if (MmarginTop === "0px") {
+        listContorls.style.marginTop = "-100vh";
+    } else {
+        if (isMobile()) {
+            PlayInfo.classList.toggle("notDesktop");
+            ContorlsPanel.classList.toggle("visiable");
+        } else {
+            playInfo.classList.toggle("vw50");
+            ContorlsPanel.classList.toggle("vw50");
+        }
+        listContorls.style.marginTop = "-100vh";
+    }
+
+})
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Ctx Contorl Slider
+const StereoPanControl = document.getElementById("stereoPanControl");
+const StereoPanValue = document.getElementById("stereoPanValue");
+
+StereoPanControl.oninput = function () {
+    stereoPanner.pan.value = StereoPanControl.value;
+    StereoPanValue.innerHTML = StereoPanControl.value;
+}
+
+
+const GainControl = document.getElementById("gainControl");
+const GainValue = document.getElementById("gainValue");
+
+GainControl.oninput = function () {
+    delayGain.gain.value = GainControl.value / 100;
+    GainValue.innerHTML = GainControl.value;
+}
+
+const FilterControl = document.getElementById("filterControl");
+const FilterValue = document.getElementById("filterValue");
+
+FilterControl.oninput = function () {
+    delayFilter.frequency.value = FilterControl.value;
+    FilterValue.innerHTML = FilterControl.value;
+}
+
+
+const DelayControl = document.getElementById("delayControl");
+const DelayValue = document.getElementById("delayValue");
+
+DelayControl.oninput = function () {
+    delayTime.delayTime.value = DelayControl.value;
+    DelayValue.innerHTML = DelayControl.value;
+}
+
+// const ReverbControl = document.getElementById("reverbControl");
+// const ReverbValue = document.getElementById("reverbValue");
+
+// ReverbControl.oninput = function () {
+//     reverb.value = ReverbControl.value;
+//     ReverbValue.innerHTML = ReverbControl.value;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Play Time Bar
+PlayProgressBar.addEventListener('input', function () {
+    audio.currentTime = this.value;
+    setTime()
+})
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Song Select & Change
+function CreareSongBook() {
+    let songbookStr = ""
+    for (i = 0; i < SongArray.length; i++) {
+        songbookStr += "<option><span>" + SongArray[i][3] + "</option></span>"
+    }
+    SongBook.innerHTML = songbookStr
+}
+
+
+SongBook.addEventListener('change', () => {
+    let idx = SongBook.selectedIndex
+    addSong(idx)
+})
+
+// SongSelect.addEventListener('dblclick', function (e) {
+//     let event = e || window.e;
+//     let target = e.target
+//     let idx
+//     if (target.nodeName.toLowerCase() != 'div') {
+//         let parent = target.parentElement;
+//         idx = Array.prototype.indexOf.call(parent.parentElement.children, parent);
+//         console.log(idx)
+//     } else ('div')
+//     addSong(idx)
+// })
+
+// const Chooses = document.querySelectorAll('.Choose')
+
+// Chooses.forEach(beChoose => {
+//     beChoose.addEventListener('click',function(e){
+//         let t = this; 
+//         let parent = t.parentElement; 
+//         let ancestor = t.parentNode.children;
+//         let i = Chooses.prototype.indexOf(t,ancestor)
+//         console.log(t)
+//         console.log(parent)
+//         console.log(ancestor)
+//         // console.log(i)
+//         // addSong(i)
+//     })
+// });
+
+var listArr = []
+var listStr = ""
+
+function addSong(idx) {
+    listArr.push(`<option ondblclick='removeSong()' value='${SongArray[idx][0]}' data-artistimgurl='${SongArray[idx][1]}' data-albumimgurl='${SongArray[idx][2]}'><span>${SongArray[idx][3]}</span></option>`)
+    ////<option ondblclick='removeSong()' value="" data-artistImgUrl="" data-albumImgUrl=""></option>
+    console.log(listArr)
+}
+
+function removeSong() {
+    let idx = SongSelected.selectedIndex;
+    SongSelected.options.remove(idx);
+    listArr.splice(idx, 1)
+    console.log(listArr)
+    arrToStr()
+}
+
+function arrToStr() {
+    listStr = listArr.join()
+    if (listStr !== "") {
+        songSelected.innerHTML = listStr
+    }
+    listStr = ""
+}
+
+SongSelected.addEventListener('change', function () {
+    SongChange(SongSelected.selectedIndex)
+})
+
+function SongChange(index) {
+    Music.src = "src\\music\\" + SongSelected.options[index].value;
+    SongSelected.options[index].selected = true;
+    btnPlay.innerHTML = "<i class=" + "material-icons" + ">pause</i>";
+    audio.load();
+    audio.play();
+    setImage(SongSelected.options[index]);
+    setName(index);
+    setTime();
+}
+
+const SongList = document.getElementById('songList')
+const ToSongBook = document.getElementById('toSongBook')
+const ReturnAndFresh = document.getElementById('returnAndFresh')
+
+ToSongBook.addEventListener('click', () => {
+    SongList.style.transform = "rotateY(180deg)"
+})
+ReturnAndFresh.addEventListener('click', () => {
+    arrToStr()
+    SongList.style.transform = "rotateY(0deg)"
+})
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Set Song Name & Image
+function setName(index) {
+    Music.title = SongSelected.options[index].innerText;
+    SongName.innerHTML = Music.title;
+}
+
+function setImage(input) {
+    AlbumImg.src = "src\\music\\" + input.dataset.albumimgurl
+    ArtistImg.src = "src\\music\\" + input.dataset.artistimgurl
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Set Time Info
+function setTime() {
+    let AudioCurTime, AudioFullTime
+
+    audio.onloadedmetadata = function () {
+        AudioCurTime = Math.floor(audio.currentTime)
+        AudioFullTime = Math.floor(audio.duration)
+        PlayProgressBar.max = AudioFullTime
+        CurTime.innerHTML = numToTime(AudioCurTime)
+        FullTime.innerHTML = numToTime(AudioFullTime)
+    }
+
+    setInterval(() => {
+        AudioCurTime = Math.floor(audio.currentTime);
+        setTimeShow(AudioCurTime);
+        PlayProgressBar.value = AudioCurTime;
+
+        if (AudioCurTime === AudioFullTime) {
+            (RandomSwitch.classList.contains("active")) ? SongRandom() : (RepeatOneSwitch.classList.contains("active")) ? RepeatOne() : (RepeatAllSwitch.classList.contains("active")) ? RepeatAll() : SongJump(1);
+        }
+    }, 1000)
+}
+
+function setTimeShow(T) {
+    CurTime.innerHTML = numToTime(T)
+}
+
+function numToTime(T) {
+    let min = Math.floor(T / 60);
+    let sec = T % 60;
+    function addZero(num) {
+        return num < 10 ? "0" + num : String(num)
+    }
+    return addZero(min) + ":" + addZero(sec);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// play btns
+function PlayPause() {
+    if (audio !== null) {
+        if (audio.paused) {
+            audio.play();
+            setName(0)
+            setTime()
+            btnPlay.innerHTML = "<i class=" + "material-icons" + ">pause</i>";
+        } else {
+            audio.pause();
+            btnPlay.innerHTML = "<i class=" + "material-icons" + ">play_arrow</i>";
+        }
+    }
+}
+
+function Stop() {
+    // audio.pause();
+    // audio.currentTime = "0";
+    audio.load();
+    btnPlay.innerHTML = "<i class=" + "material-icons" + ">play_arrow</i>";
+}
+
+function ChangeTime(s) {
+    audio.currentTime += s
+}
+
+function SongJump(n) {
+
+    if (RandomSwitch.classList.contains("active")) {
+        SongRandom()
+    } else {
+        let indexChange = SongSelected.selectedIndex + n;
+
+        if (indexChange === (SongSelected.length)) {
+            SongChange(0)
+        }
+        if (indexChange === -1) {
+            SongChange(SongSelected.length - 1)
+        }
+        SongChange(indexChange)
+    }
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// repeat btns
+const RandomSwitch = document.getElementById('randomSwitch');
+const RepeatOneSwitch = document.getElementById('repeatOneSwitch');
+const RepeatAllSwitch = document.getElementById('repeatAllSwitch');
+
+RandomSwitch.addEventListener('click', function () {
+    this.classList.toggle('active')
+    RepeatOneSwitch.classList.remove('active')
+    RepeatAllSwitch.classList.remove('active')
+})
+
+function SongRandom() {
+    let r = Math.round(Math.random() * SongSelected.length)
+    SongChange(r)
+    console.log(r)
+}
+
+RepeatOneSwitch.addEventListener('click', function () {
+    this.classList.toggle('active')
+    RandomSwitch.classList.remove('active')
+    RepeatAllSwitch.classList.remove('active')
+})
+
+function RepeatOne() {
+    audio.currentTime = 0;
+    audio.play()
+}
+
+RepeatAllSwitch.addEventListener('click', function () {
+    this.classList.toggle('active')
+    RandomSwitch.classList.remove('active')
+    RepeatOneSwitch.classList.remove('active')
+})
+
+function RepeatAll() {
+    ((SongSelected.selectedIndex + 1) === SongSelected.length) ? SongChange(0) : SongJump(1)
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Volume Contorl
+function VolumeSwitch() {
+    // audio.muted = audio.muted == true ? false : true 
+    audio.muted = !audio.muted;
+    MusicVolume.textContent === "volume_up" ? MusicVolume.innerHTML = "volume_off" : MusicVolume.innerHTML = "volume_up"
+}
+
+VolumeControl.oninput = function () {
+    masterVolume.gain.value = this.value / 100;
+    VolumeValue.innerHTML = this.value;
+    masterVolume.gain.value === 0 ? MusicVolume.innerHTML = "volume_off" : MusicVolume.innerHTML = "volume_up"
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// window.onload init
+window.onload = function () {
+    CreareSongBook()
+    setImage(music);
+    SongName.innerHTML = Music.title;
+}
