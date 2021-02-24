c66821293716f8eff0f510415fabdc07270886e4
afterfireworks <afterfireworks@users.noreply.github.com>
2021-02-24 22:05:27 +0800

    20210224 2204
    Add Collage*3

--- src/Extend/Basic/NoisyPlayer with WebAudioAPI/index.js
+++ src/Extend/Basic/NoisyPlayer with WebAudioAPI/index.js
@@ -6,7 +6,10 @@ const SongArray = [
     ["Vast&Hazy-IntheDark.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ep02.jpg", "banner1.jpg", "Vast & Hazy - 無差別傷害"],
     ["Vast&Hazy-Waves.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ep01.jpg", "banner3.jpg", "Vast & Hazy - 與浪之間"],
     ["Vast&Hazy-TheCityisEatingMeAlive.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ep01.jpg", "banner3.jpg", "Vast & Hazy - 食人夢"],
-    ["Vast&Hazy-ImNotOK.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ab01.jpg", "banner3.jpg", "Vast & Hazy - 求救訊號"]
+    ["Vast&Hazy-ImNotOK.mp3", "Vast&Hazy.artist.jpg", "Vast&Hazy.ab01.jpg", "banner3.jpg", "Vast & Hazy - 求救訊號"],
+    ["Collage-tsòng hōo kui-lōo hué-hu iû-tsāi.mp3", "Collage.artist.jpg", "Collage.01.jpg", "banner2.jpg", "珂拉琪 - 葬予規路火烌猶在"],
+    ["Collage - Bān-tshian Hue-luí Tsû-bió Pi-ai.mp3", "Collage.artist.jpg", "Collage.02.jpg", "banner2.jpg", "珂拉琪 - 萬千花蕊慈母悲哀"],
+    ["Collage - tse kai-sí ê khu-tsip kah ài.mp3", "Collage.artist.jpg", "Collage.03.jpg", "banner2.jpg", "珂拉琪 - 這該死的拘執佮愛"]
 ]
 
 const audio = document.querySelector('audio');
